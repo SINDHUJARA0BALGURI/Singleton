ea37d74aa53f205f2d4ab11bf8716ff5f215ab6a
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-08-30 16:40:40 +0530

    instatiated cube

--- Assets/GamePrefabEnvironment.cs
+++ Assets/GamePrefabEnvironment.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GamePrefabEnvironment : MonoBehaviour
 {
+    public int width, depth;
+    public GameObject cube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < width; i++)
+        {
+            for(int x = 0; x < depth; x++)
+            {
+                Vector3 pos = new Vector3(i, 0, x);
+                Instantiate(cube, pos, Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
