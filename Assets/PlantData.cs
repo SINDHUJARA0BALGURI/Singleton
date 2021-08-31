2f917dd466e7bde670628801eb233c860cf5dff0
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-08-31 15:19:04 +0530

    Scriptable objects is done and added to plant prefabs

---
+++ Assets/PlantData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="plant data", menuName ="PlantData",order =60)]
+public class PlantData : ScriptableObject
+{
+    public enum THREAT { None,Low,Moderate,High}
+    [SerializeField]
+    private string plantName;
+    [SerializeField]
+    private THREAT plantThreat;
+    [SerializeField]
+    private Texture icon;
+}
